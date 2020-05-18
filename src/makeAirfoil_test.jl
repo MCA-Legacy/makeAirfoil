c5966fb15e44791bd455d44c8e01d99ec27a6777
Mark C. Anderson <anderson.mark.az@gmail.com>
2020-05-18 11:50:51 -0600

    Updated the angle range

--- src/makeAirfoil_test.jl
+++ src/makeAirfoil_test.jl
@@ -5,7 +5,7 @@ import makeAirfoil.tabulateData
 
 airfoil = naca(2,4,12,0.03)
 
-angleRange = -10:0.5:10
+angleRange = -20:0.5:30
 
 path = string(homedir(),"/Box/FLOW-MCA/FLOW-Code/Repositories/personal-projects/NonlinearLiftingLine/src/airfoil-data")
 airfoilName = "NACA2412"
